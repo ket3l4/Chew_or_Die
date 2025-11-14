f17a8ba96e1403d756a3f8b2611ab0475d0c61a2
Arnold Stanovský <stanoarn@fit.cvut.cz>
2025-11-14 15:17:04 +0800

    UV init, some silliness

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # eel
 A 3D extension of Snake written in Python
+
+![funny eel](./assets/eel.jpg "A funny eel")
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("How are you f_eel_ing today?")
+
+
+if __name__ == "__main__":
+    main()
